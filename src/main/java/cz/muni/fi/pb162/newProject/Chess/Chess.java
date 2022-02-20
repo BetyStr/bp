4a6f90bc67ad5bb25a4396772a4b464ee211b93b
Alžbeta Strompová <xstromp@fi.muni.cz>
2022-02-20 13:47:58 +0100

    Add abstract class Game.java

---
+++ src/main/java/cz/muni/fi/pb162/newProject/Chess/Chess.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pb162.newProject.Chess;
+
+import cz.muni.fi.pb162.newProject.Board;
+import cz.muni.fi.pb162.newProject.Game;
+
+/**
+ * @author Alzbeta Strompova
+ */
+public class Chess extends Game {
+
+
+    @Override
+    public Board setInitialSet() {
+        return null;
+    }
+}
