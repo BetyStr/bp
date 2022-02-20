98c42daa4aecce2e61740a1d7045fa2ffe6ee811
Alžbeta Strompová <xstromp@fi.muni.cz>
2022-02-20 13:46:44 +0100

    Add Player.java and Coordinates.java

---
+++ src/main/java/cz/muni/fi/pb162/newProject/Coordinates.java
@@ -0,0 +1,47 @@
+package cz.muni.fi.pb162.newProject;
+
+import java.util.Objects;
+
+/**
+ * @author Alzbeta Strompova
+ */
+public class Coordinates {
+
+    private final int x;
+    private final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Coordinates point = (Coordinates) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
---
+++ src/main/java/cz/muni/fi/pb162/newProject/Player.java
@@ -0,0 +1,26 @@
+package cz.muni.fi.pb162.newProject;
+
+/**
+ * @author Alzbeta Strompova
+ */
+public class Player {
+
+    private String name;
+    private Color color;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
