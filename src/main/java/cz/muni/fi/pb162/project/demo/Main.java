75df1a63eacd21ec613786f5b50f1604f8c46a9b
Alžbeta Strompová <xstromp@fi.muni.cz>
2022-04-10 19:29:55 +0200

    Add Hello World to main

--- src/main/java/cz/muni/fi/pb162/project/demo/Main.java
+++ src/main/java/cz/muni/fi/pb162/project/demo/Main.java
@@ -1,11 +1,5 @@
 package cz.muni.fi.pb162.project.demo;
 
-import cz.muni.fi.pb162.project.Chess;
-import cz.muni.fi.pb162.project.Color;
-import cz.muni.fi.pb162.project.Player;
-import cz.muni.fi.pb162.project.excepions.EmptySquareException;
-import cz.muni.fi.pb162.project.excepions.NotAllowedMoveException;
-
 /**
  * Class for running main method.
  *
@@ -19,13 +13,8 @@ public class Main {
      *
      * @param args command line arguments, will be ignored.
      */
-    public static void main(String[] args) throws EmptySquareException, NotAllowedMoveException {
-        var player1 = new Player("Jozko", Color.WHITE);
-        var player2 = new Player("Janko", Color.BLACK);
-
-        var chess = new Chess(player1, player2);
-        chess.setInitialSet();
-        chess.play();
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 
 }
