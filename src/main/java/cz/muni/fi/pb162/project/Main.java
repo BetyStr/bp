4cbfb331f8178e046387107bd1ba395c48a58638
Alžbeta Strompová <xstromp@fi.muni.cz>
2022-05-10 19:17:23 +0200

    Initial commit

--- src/main/java/cz/muni/fi/pb162/project/Main.java
+++ src/main/java/cz/muni/fi/pb162/project/Main.java
@@ -1,34 +1,20 @@
 package cz.muni.fi.pb162.project;
 
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
 /**
+ * Class for running main method.
+ *
  * @author Alzbeta Strompova
  */
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello world!");
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("B");
-        System.out.println(dtf.withLocale(Locale.ENGLISH).format(LocalTime.of(15, 0)));
 
-        System.out.println(System.lineSeparator());
-        var player1 = new Player("Jozko", Color.White);
-        var player2 = new Player("Janko", Color.Black);
-
-        var chess = new Chess(player1, player2);
-
-        chess.setInitialSet();
-        System.out.println(chess.printBoardToConsole());
-//        var guiBoard = new GUIBoard();
-//        guiBoard.display(chess);
-
-//        chess.write(System.out);
-        chess.play();
-
-
+    /**
+     * Runs the code.
+     *
+     * @param args command line arguments, will be ignored.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 
 }
